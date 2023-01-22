d287057d6cee727f0ee837ddd9d5c46a3f195f18
Kamil Nowinski <kamil@nowinski.net>
2023-01-22 00:36:58 +0000

    ver. 2.122.0036

--- src/MADS/ver.asm
+++ src/MADS/ver.asm
@@ -1,5 +1,5 @@
 .MACRO VER
     dta c'-== Dangerous City ==-'
-    dta c'    ver. 2.114.2201   '   ; 2=ver 2023 / 114 - MDD / 2201 - HHMM of build
+    dta c'    ver. 2.122.0036   '   ; 2=ver 2023 / 114 - MDD / 2201 - HHMM of build
     dta c'    Kamil Nowinski    '
 .ENDM
