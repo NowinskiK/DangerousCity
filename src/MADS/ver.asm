0af937f059302a0eba0ebf26e6b894d22aeb5ea0
Kamil Nowinski <kamil@nowinski.net>
2023-01-22 11:37:58 +0000

    version for debug_mode

--- src/MADS/ver.asm
+++ src/MADS/ver.asm
@@ -1,6 +1,10 @@
      dta c'-== Dangerous City ==-'
      dta c'    Kamil Nowinski    '
+.if DEBUG_MODE
+ver1 dta d '    version (debug mode on):    '
+.else
 ver1 dta d '             version:           '
+.endif
 ver2 dta d '            2.122.1120          '
 
 ; 2=ver 2023 / 114 - MDD / 2201 - HHMM of build
